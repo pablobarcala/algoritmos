1b0382dd463325c87a1f2baf1b9d6bc3dd6d5aef
Pablo Barcala <pablobarcala4@gmail.com>
2023-08-23 17:20:46 -0300

    ejercicio 14

---
+++ tp4/14.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, c;
+
+    printf("Ingrese 3 numeros:");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b){
+        if(b > c){
+            printf("%d %d %d", c,b,a);
+        } else if(a > c){
+            printf("%d %d %d", b,c,a);
+        } else {
+            printf("%d %d %d", b,a,c);
+        }
+    } else {
+        if(b < c){
+            printf("%d %d %d", a,b,c);
+        } else if(a > c){
+            printf("%d %d %d", c,a,b);
+        } else {
+            printf("%d %d %d", a,c,b);
+        } 
+    }     
+
+    return 0;
+}
