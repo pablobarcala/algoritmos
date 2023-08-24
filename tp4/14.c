46fdb3574f9ddad0330031b89a27bb0ea8e41d7d
Pablo Barcala <pablobarcala4@gmail.com>
2023-08-24 09:51:44 -0300

    mostrar si son iguales en ejercicio 14

--- tp4/14.c
+++ tp4/14.c
@@ -6,23 +6,27 @@ int main(){
     printf("Ingrese 3 numeros:");
     scanf("%d %d %d", &a, &b, &c);
 
-    if(a > b){
-        if(b > c){
-            printf("%d %d %d", c,b,a);
-        } else if(a > c){
-            printf("%d %d %d", b,c,a);
-        } else {
-            printf("%d %d %d", b,a,c);
-        }
+    if(a == b == c){
+        printf("Son iguales %d %d %d", a,b,c);
     } else {
-        if(b < c){
-            printf("%d %d %d", a,b,c);
-        } else if(a > c){
-            printf("%d %d %d", c,a,b);
+        if(a > b){
+            if(b > c){
+                printf("%d %d %d", c,b,a);
+            } else if(a > c){
+                printf("%d %d %d", b,c,a);
+            } else {
+                printf("%d %d %d", b,a,c);
+            }
         } else {
-            printf("%d %d %d", a,c,b);
-        } 
-    }     
+            if(b < c){
+                printf("%d %d %d", a,b,c);
+            } else if(a > c){
+                printf("%d %d %d", c,a,b);
+            } else {
+                printf("%d %d %d", a,c,b);
+            } 
+        }   
+    }
 
     return 0;
 }
