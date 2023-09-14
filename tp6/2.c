c5e3ca0dcbceb30abfd4a5351aaa50300f1af560
Pablo Barcala <pablobarcala4@gmail.com>
2023-09-14 11:49:58 -0300

    tp6 ejercicio 2

---
+++ tp6/2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int cargarVect(int M, int v[]);
+
+int main(){
+    int M, N = 0, P = 0;
+
+    printf("Ingrese la cantidad de numeros: ");
+    scanf("%d", &M);
+
+    int vector[M];
+    cargarVect(M, vector);
+
+    for(int i = 0; i < M; i++){
+        if(vector[i] < 0) {
+            N++;
+        } else if(vector[i] > 0){
+            P++;
+        }
+    }
+
+    printf("Cantidad de valores negativos: %d\nCantidad de valores positivos: %d", N, P);
+
+    return 0;
+}
+
+int cargarVect(int M, int v[]){
+    int i;
+    for(i = 0; i < M; i++){
+        printf("Ingrese un numero: ");
+        scanf("%d", &v[i]);
+    }
+}
