51094c5c26400c325eafe81d3e2ad98905bd0aa9
Pablo Barcala <pablobarcala4@gmail.com>
2023-09-14 11:52:15 -0300

    tp6 ejercicio 3

---
+++ tp6/3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int cargarVect(int N, int v[]);
+
+int main(){
+    int N, mayor, posicion;
+
+    printf("Ingrese la cantidad de numeros: ");
+    scanf("%d", &N);
+
+    int vector[N];
+    cargarVect(N, vector);
+
+    for(int i = 0; i < N; i++){
+        if(i == 0){
+            mayor = vector[i];
+            posicion = i;
+        } else if(vector[i] > mayor){
+            mayor = vector[i];
+            posicion = i;
+        }
+    }
+
+    printf("El mayor numero es %d y esta en la posicion %d", mayor, posicion);
+
+    return 0;
+}
+
+int cargarVect(int N, int v[]){
+    int i;
+    for(i = 0; i < N; i++){
+        printf("Ingrese un numero: ");
+        scanf("%d", &v[i]);
+    }
+}
