531028a3db1c0ad1be95eeb511ae172e798b9865
Pablo Barcala <pablobarcala4@gmail.com>
2023-09-14 16:40:47 -0300

    tp6 ejercicio 5

---
+++ tp6/5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int cargarVect(int N, int v[]);
+
+int main(){
+    int N;
+
+    printf("Ingrese la cantidad de numeros: ");
+    scanf("%d", &N);
+
+    int vector[N];
+    cargarVect(N, vector);
+
+    for(int i = 0; i < N; i++){
+        printf("%d\n", vector[i]);
+    }
+
+    return 0;
+}
+
+int cargarVect(int N, int v[]){
+    int adicional = 0, i;
+    for(i = N-1; i >= 0; i--){
+        v[i] = N + adicional;
+        adicional++;
+    }
+}
