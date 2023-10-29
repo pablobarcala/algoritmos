b211bcdba7977b7e0cfc4d756d5f42d7b5750150
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-29 10:47:26 -0300

    cambiar numero int por char para no tener problemas

--- ejercicio/ejercicio.c
+++ ejercicio/ejercicio.c
@@ -4,12 +4,11 @@
 typedef struct {
     char nombre[20];
     char apellido[40];
-    int numero;
+    char numero[20];
 } contacto;
 
 void cargarContacto(contacto nuevoContacto, FILE *archivo);
 void mostrarContactos(contacto nuevoContacto, FILE *archivo);
-void eliminarContacto(contacto contacto, FILE *archivo);
 
 int main(){
     contacto nuevoContacto;
@@ -17,7 +16,7 @@ int main(){
     int opcion;
 
     do { 
-        printf("\nSeleccione una opcion:\n1. Cargar contacto\n2. Mostrar contactos\n3. Eliminar un contacto\n4. Salir\n");
+        printf("\nSeleccione una opcion:\n1. Cargar contacto\n2. Mostrar contactos\n3. Salir\n");
         scanf("%d", &opcion);
         fflush(stdin);
 
@@ -28,12 +27,9 @@ int main(){
             case 2:
                 mostrarContactos(nuevoContacto, archivo);
                 break;
-            case 3:
-                eliminarContacto(nuevoContacto, archivo);
-                break;
         }
 
-    } while (opcion != 4);
+    } while (opcion != 3);
 
     return 0;
 }
@@ -52,7 +48,8 @@ void cargarContacto(contacto nuevoContacto, FILE *archivo){
     fgets(nuevoContacto.apellido, sizeof(nuevoContacto.apellido), stdin);
     fflush(stdin);
     printf("\nIngrese el numero: ");
-    scanf("%d", &nuevoContacto.numero);
+    fgets(nuevoContacto.numero, sizeof(nuevoContacto.numero), stdin);
+    fflush(stdin);
 
     fseek(archivo, 0, 2);
     fwrite(&nuevoContacto, sizeof(nuevoContacto), 1, archivo);
@@ -71,31 +68,7 @@ void mostrarContactos(contacto nuevoContacto, FILE *archivo){
         printf("--------------\n");
         printf("Nombre: %s\n", nuevoContacto.nombre);
         printf("Apellido: %s\n", nuevoContacto.apellido);
-        printf("Numero: %d\n", nuevoContacto.numero);
+        printf("Numero: %s\n", nuevoContacto.numero);
         fread(&nuevoContacto, sizeof(nuevoContacto), 1, archivo);
     }
-}
-
-void eliminarContacto(contacto contacto, FILE *archivo){
-    int numeroEliminar;
-
-    archivo = fopen("C:/algoritmos/ejercicio/archivos/contactos.dat", "rb+");
-    if(archivo == NULL){
-        archivo = fopen("C:/algoritmos/ejercicio/archivos/contactos.dat", "wb+");
-    }
-
-    printf("Ingrese el numero del contacto a eliminar: ");
-    scanf("%d", &numeroEliminar);
-
-    fseek(archivo, 0, 0);
-    while(fread(&contacto, sizeof(contacto), 1, archivo) > 0){
-        if(numeroEliminar == contacto.numero){
-            printf("\nSe elimino el contacto con el numero ingresado\n");
-        } else {
-            fwrite(&contacto, sizeof(contacto), 1, archivo);
-        }
-    }
-
-    remove("archivos/contactos.dat");
-    rename("archivos/contactos.dat", "archivos/contactos.dat");
 }
